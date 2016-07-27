fb94ab518ff42dc1ab9201d847f820a34b6f58a3
inikulin <ifaaan@gmail.com>
2016-07-27 13:05:40 +0300

    Implement warnings. New error format. Bump version.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 export default function () {
     return {
-        noColors:     false,
-        startTime:    null,
-        afterErrList: false,
-        testCount:    0,
+        noColors:       false,
+        startTime:      null,
+        afterErrorList: false,
+        testCount:      0,
 
         reportTaskStart (startTime, userAgents, testCount) {
-            var uaList = userAgents
-                .map(ua => this.chalk.blue(ua))
-                .join(', ');
-
             this.startTime = startTime;
             this.testCount = testCount;
 
-            this.setIndent(0)
+            this.setIndent(1)
                 .useWordWrap(true)
-                .write(this.chalk.bold(`Running tests in: ${uaList}`))
+                .write(this.chalk.bold('Running tests in:'))
                 .newline();
+
+            userAgents.forEach(ua => {
+                this
+                    .write(`- ${this.chalk.blue(ua)}`)
+                    .newline();
+            });
         },
 
         reportFixtureStart (name) {
             this.setIndent(1)
                 .useWordWrap(true);
 
-            if (this.afterErrList)
-                this.afterErrList = false;
+            if (this.afterErrorList)
+                this.afterErrorList = false;
             else
                 this.newline();
 
@@ -32,60 +34,82 @@ export default function () {
                 .newline();
         },
 
-        reportTestDone (name, errs, durationMs, unstable, screenshotPath) {
-            var hasErr    = !!errs.length;
-            var nameStyle = hasErr ? this.chalk.red.bold : this.chalk.gray;
+        _renderErrors (errs) {
+            this.setIndent(3)
+                .newline();
+
+            errs.forEach((err, idx) => {
+                var prefix = this.chalk.red(`${idx + 1}) `);
+
+                this.newline()
+                    .write(this.formatError(err, prefix))
+                    .newline()
+                    .newline();
+            });
+        },
+
+        reportTestDone (name, testRunInfo) {
+            var hasErr    = !!testRunInfo.errs.length;
             var symbol    = hasErr ? this.chalk.red.bold(this.symbols.err) : this.chalk.green(this.symbols.ok);
+            var nameStyle = hasErr ? this.chalk.red.bold : this.chalk.grey;
             var title     = `${symbol} ${nameStyle(name)}`;
 
-            this.setIndent(2)
+            this.setIndent(1)
                 .useWordWrap(true);
 
-            if (unstable)
+            if (testRunInfo.unstable)
                 title += this.chalk.yellow(' (unstable)');
 
-            if (screenshotPath)
-                title += ` (screenshots: ${this.chalk.underline(screenshotPath)})`;
+            if (testRunInfo.screenshotPath)
+                title += ` (screenshots: ${this.chalk.underline.grey(testRunInfo.screenshotPath)})`;
 
             this.write(title);
 
-            if (hasErr) {
-                this.setIndent(4)
-                    .newline();
+            if (hasErr)
+                this._renderErrors(testRunInfo.errs);
 
-                errs.forEach((err, idx) => {
-                    var prefix = this.chalk.red(`${idx + 1}) `);
-
-                    this.newline()
-                        .write(this.formatError(err, prefix))
-                        .newline()
-                        .newline();
-                });
-            }
-
-            this.afterErrList = hasErr;
+            this.afterErrorList = hasErr;
 
             this.newline();
         },
 
-        reportTaskDone (endTime, passed) {
+        _renderWarnings (warnings) {
+            this.newline()
+                .setIndent(1)
+                .write(this.chalk.bold.yellow(`Warnings (${warnings.length}):`))
+                .newline();
+
+            warnings.forEach(msg => {
+                this.setIndent(1)
+                    .write(this.chalk.bold.yellow(`--`))
+                    .newline()
+                    .setIndent(2)
+                    .write(msg)
+                    .newline();
+            });
+        },
+
+        reportTaskDone (endTime, passed, warnings) {
             var durationMs  = endTime - this.startTime;
             var durationStr = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
             var footer      = passed === this.testCount ?
                               this.chalk.bold.green(`${this.testCount} passed`) :
                               this.chalk.bold.red(`${this.testCount - passed}/${this.testCount} failed`);
 
-            footer += this.chalk.gray(` (${durationStr})`);
+            footer += this.chalk.grey(` (${durationStr})`);
+
+            if (!this.afterErrorList)
+                this.newline();
 
             this.setIndent(1)
                 .useWordWrap(true);
 
-            if (!this.afterErrList)
-                this.newline();
-
             this.newline()
                 .write(footer)
                 .newline();
+
+            if (warnings.length)
+                this._renderWarnings(warnings);
         }
     };
 }
